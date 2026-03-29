af29270e51e7c12a66e7b07be90ddbfffb6be700
Alice <alicesinhasousa7@gmail.com>
2026-03-29 10:37:55 -0300

    chore: add eslint, prettier and path aliases

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,15 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
-};
+  extends: [
+    '@react-native',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  rules: {
+    'no-console': 'warn',
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-explicit-any': 'warn',
+    'react-native/no-inline-styles': 'warn',
+  },
+}
